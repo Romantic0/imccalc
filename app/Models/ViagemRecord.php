fbf3ecf1d6b2454d6396f2b667cee149318bd9b9
Guilherme Afonso Casa <guylhermeeafonso@gmail.com>
2025-03-26 13:53:16 -0300

    finalizado

--- app/Models/ViagemRecord.php
+++ app/Models/ViagemRecord.php
@@ -4,15 +4,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory; // <-- Aqui está a importação
+use Illuminate\Database\Eloquent\Factories\HasFactory; // 
 use Illuminate\Database\Eloquent\Model;
 
 class ViagemRecord extends Model
 {
-    use HasFactory; // <-- Aqui você usa a trait
+    use HasFactory; // 
 
     protected $fillable = ['distancia', 'preco_gasolina', 'consumo_veiculo', 'gasto'];
 
-    // Se precisar de alguma outra lógica adicional, você pode adicioná-la aqui
+    
 }
 
